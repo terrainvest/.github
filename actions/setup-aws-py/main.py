699658ea4f3844911eec798cc72845dae32d9d4a
murilopacsonov <murilo.portilho@terrainvestimentos.com.br>
2021-01-14 17:32:43 -0300

    python action change run

--- actions/setup-aws-py/main.py
+++ actions/setup-aws-py/main.py
@@ -8,9 +8,9 @@ def main(aws_profile, root_path):
     try:
         os.mkdir(f"{root_path}.aws")
     except OSError:
-        print (f"Creation of the directory {root_path}/.aws failed")
+        print (f"Creation of the directory {root_path}.aws failed")
     else:
-        print (f"Successfully created the directory {root_path}/.aws")
+        print (f"Successfully created the directory {root_path}.aws")
 
     ORG_TF_AWS_KEY_ID = os.environ["ORG_TF_AWS_KEY_ID"]
     ORG_TF_AWS_SECRET_KEY = os.environ["ORG_TF_AWS_SECRET_KEY"]
@@ -21,7 +21,7 @@ def main(aws_profile, root_path):
     PRD_TF_AWS_KEY_ID = os.environ["PRD_TF_AWS_KEY_ID"]
     PRD_TF_AWS_SECRET_KEY = os.environ["PRD_TF_AWS_SECRET_KEY"]    
 
-    fileAws = open(f"{root_path}/.aws/credentials","a+")
+    fileAws = open(f"{root_path}.aws/credentials","a+")
 
     fileAws.write("[org]")
     fileAws.write(f"aws_access_key_id = { ORG_TF_AWS_KEY_ID }")
@@ -40,6 +40,8 @@ def main(aws_profile, root_path):
     fileAws.write(f"aws_secret_access_key = { PRD_TF_AWS_SECRET_KEY }")
 
     fileAws.close()    
+
+    os.environ["AWS_PROFILE"] = aws_profile
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
