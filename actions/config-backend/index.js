7fb8c283bff398fff210dce2d3521556cc24d091
MuriloAcsonov <murilloacsonovv@hotmail.com>
2021-01-18 12:09:28 -0300

    create new action

---
+++ actions/config-backend/index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+
+const path = core.getInput('path');
+
+
+async function getKey(path) {
+  try {
+    
+    let arrayPath = path.split("/")
+    let key = ""
+
+    for (let index = 2; index <= arrayPath.length - 1; index++){
+      key += `${arrayPath[index]}/`
+    }
+
+    return key
+
+  }catch (err){     
+     core.setFailed(err);
+  };
+};
+
+getKey(path)
+  .then(
+    key => setOutput(key)
+  )
+
+  async function setOutput(key) {
+    
+    core.setOutput("key", key);
+    console.log(`output key: ${key}`)
+
+  }
