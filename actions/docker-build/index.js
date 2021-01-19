dcb83884f9822b9b712d9b31fc191f516b90e751
murilopacsonov <murilo.portilho@terrainvestimentos.com.br>
2021-01-19 16:32:25 -0300

    update index action docker build

--- actions/docker-build/index.js
+++ actions/docker-build/index.js
@@ -13,6 +13,10 @@ async function run(){
 
     dotenv.config({path: `${__dirname}/.github/.env.lambda` });
 
+    fs.readdirSync(`${__dirname}/.github`).forEach(file => {
+        console.log(file);
+      });
+
     if(fs.existsSync(`${__dirname}/.github`)){
         console.log("EXISTE");
     }
