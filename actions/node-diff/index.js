bfea6ed2febf4b0b750767ad9cdbded014316aac
murilopacsonov <murilo.portilho@terrainvestimentos.com.br>
2021-01-22 18:13:02 -0300

    update index js node-diff

--- actions/node-diff/index.js
+++ actions/node-diff/index.js
@@ -21,7 +21,7 @@ async function getPath() {
 
         console.error(`Nothing return from git diff-tree command at commit: ${currentCommit}`)
 
-        let { stdout2 } = await exec(`git log --pretty=%P -n 1 ${currentCommit}`);
+        let { stdout2 } = await exec(`git log -3 --format='%h`);
 
         console.log(stdout2)
 
