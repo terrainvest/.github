7a34bafa6ea957b2efbe6e9c6fd3768039415a9c
murilopacsonov <murilo.portilho@terrainvestimentos.com.br>
2021-01-26 18:32:37 -0300

    update action

--- actions/create-package/main.py
+++ actions/create-package/main.py
@@ -26,11 +26,13 @@ def main():
     pass
 
 def updImage(client, functionName):
+    print(f"Update function {functionName}")
     client.update_function_code(FunctionName=functionName, ImageUri=args.image)
 
     pass
 
 def updS3(client, functionName, keyS3):
+    print(f"Update function {functionName}")
     client.update_function_code(FunctionName=functionName, S3Bucket=f"default.lambda.package.{args.profile}", S3Key=keyS3)
 
     pass
@@ -38,17 +40,24 @@ def updS3(client, functionName, keyS3):
 def createPackage():
     zipName = f"{os.environ['GITHUB_SHA'][:8]}.zip"
 
+    print("Creating zip file from ./dist folder")    
     with ZipFile(zipName, 'w') as zipObj:
         for folderName, subfolders, filenames in os.walk('./dist'):
             for filename in filenames:
                 filePath = os.path.join(folderName, filename)
                 zipObj.write(filePath, basename(filePath))
 
+    print(f"Zip Name: {zipName}")
+
     sessionAws = boto3.Session(profile_name=args.profile, region_name='us-east-1')
     clientS3 = sessionAws.client('s3')
 
     try:
+        print(f"Starting upload file to s3...")
         response = clientS3.upload_file(zipName, f"default.lambda.package.{args.profile}", zipName)
+        print(f"Finished upload.")
+        print(f"Bucket: default.lambda.package.{args.profile}")
+        print(f"Object: {zipName}")
     except ClientError as e:
         print(f"Error: {e}")
 
@@ -59,10 +68,13 @@ def createPackage():
 if __name__ == '__main__':
 
     functionName = f"{os.environ['GITHUB_REPOSITORY'].split('/')[1]}-{args.profile}" if args.profile != 'prd' else os.environ['GITHUB_REPOSITORY'].split('/')[1]
-    client = main()
+    print(f"Function to update: {functionName}")
+    client = main()   
 
     if args.docker == 'true':
+        print(f"We are update function using a docker image...")
         updImage(client, functionName)
     else:
-        zipName = createPackage()
+        print(f"We will create a zip and upload to S3...")
+        zipName = createPackage()        
         updS3(client, functionName, zipName)
