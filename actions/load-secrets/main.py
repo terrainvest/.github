3bb0afc2c466a6193634cc141e7bc0b413fcb222
murilopacsonov <murilo.portilho@terrainvestimentos.com.br>
2021-02-03 12:17:05 -0300

    upd templates

---
+++ actions/load-secrets/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import subprocess
+
+
+def main():
+    jsonRaw = os.getenv["SECRETS_CONTEXT"]
+    jsonData = json.load(jsonRaw)
+
+    for key, value in jsonData.items():
+        os.system(f'echo "{key}={value}" >> $GITHUB_ENV')
+
+if __name__ == '__main__':        
+    main()
